b26b651afd840919fec42733a6bd677f935b9ffc
joker314 <maxzuba22@yahoo.co.uk>
2016-12-23 16:19:54 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 <head></head>
 <body>
 <?php
-if($_POST["sent"] == "yes"){
-  echo "<div>We aren't quite ready</div>";
-}
+echo "<div>The PHP now works</div>";
 ?>
 To modify the order list, you will need to provide some authentication. Type the password that you were given below
 <form action="index.php" method="post">
